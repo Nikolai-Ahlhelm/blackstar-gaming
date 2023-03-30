3361bcd781578c9077380e2ec55522d3245d1709
Nikolai <ahlhelm.n.01@gmail.com>
2023-03-30 21:46:51 +0200

    Vorbereitung für Custom Domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://Nikolai-Ahlhelm.github.io',
+  url: 'https://blackstar-gaming.de',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/blackstar-gaming/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Nikolai-Ahlhelm', // Usually your GitHub org/user name.
   projectName: 'blackstar-gaming', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
